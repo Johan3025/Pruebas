def8d82dacce26fdd23dafa280eef893978c3984
johan <arjohan1822@hotmail.com>
2018-09-24 01:26:23 -0500

    CAMBIO

---
+++ PedidosProductosRodando/IService1.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PedidosProductosRodando
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+        //[OperationContract]
+        //bool AgregarPedido (Pedidos Pedido);
+
+        //[OperationContract]
+        //bool BorrarPedido (Pedidos Pedido);
+
+        //[OperationContract]
+        //bool EditarPedido(Pedidos Pedido);
+
+        [OperationContract]
+        List<Employees> DisplayEmployees();
+
+        
+
+        [OperationContract]
+        Employees SearchEmployees(int id);
+    }
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    // Puede agregar archivos XSD al proyecto. Después de compilar el proyecto, puede usar directamente los tipos de datos definidos aquí, con el espacio de nombres "PedidosProductosRodando.ContractType".
+    
+}
---
+++ PedidosProductosRodando/Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PedidosProductosRodando
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class Service1 : IService1
+    {
+        EXAMPLEEntities1 db = new EXAMPLEEntities1();
+        Employees detail;
+
+        public List<Employees> DisplayEmployees()
+        {
+
+            var test = db.Employees.ToList();
+
+            return test;
+        }
+
+        public Employees SearchEmployees(int id)
+        {
+            try
+            {
+                int iden = Convert.ToInt32(id);
+
+                detail = db.Employees.Find(iden);
+                return detail;
+
+            } catch (Exception ex)
+            {
+
+            }
+            return detail;
+        }
+    }
+}
